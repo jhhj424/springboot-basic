ed8918837230ab5b0422aba5dcbee34fbcfdf435
jiho shin <jhhj424@naver.com>
2021-02-05 01:26:09 +0900

    fix: Typo correction

--- src/main/java/jito/web/springbootbasic/logger/LoggingController.java
+++ src/main/java/jito/web/springbootbasic/logger/LoggingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoggingController {
 
-    @Value("${logging-module.version")
+    @Value("${logging-module.version}")
     private String version;
 
     @GetMapping("/")
