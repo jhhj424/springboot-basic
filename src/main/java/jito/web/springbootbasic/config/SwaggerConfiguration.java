298b8291fa5ed229fa6a17d8d72747ed71461ce9
jiho shin <jhhj424@naver.com>
2021-02-08 23:34:34 +0900

    feat: Added swagger [Swagger를 적용하여 쉽게 api를 볼 수 있도록 함]

---
+++ src/main/java/jito/web/springbootbasic/config/SwaggerConfiguration.java
@@ -0,0 +1,30 @@
+package jito.web.springbootbasic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("jito.web.springbootbasic.member.ui"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("Spring API Documentation")
+                .description("지토의 스프링부트 기본 프로젝트 입니다.")
+                .license("개발하는 지토").licenseUrl("http://github.com/jhhj424").version("1").build();
+    }
+}
--- src/main/java/jito/web/springbootbasic/member/ui/MemberController.java
+++ src/main/java/jito/web/springbootbasic/member/ui/MemberController.java
@@ -1,15 +1,19 @@
 package jito.web.springbootbasic.member.ui;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jito.web.springbootbasic.member.domain.Member;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"1. Member"})
 @RestController
 @RequestMapping("/members")
 public class MemberController {
 
+    @ApiOperation(value = "샘플", notes = "테스트용 api")
     @GetMapping("/sample")
     public ResponseEntity<Member> sampleFindMember() {
         Member member = new Member("테스트 사용자", 10);
